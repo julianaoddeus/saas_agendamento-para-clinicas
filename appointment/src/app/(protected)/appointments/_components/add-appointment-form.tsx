31d315c79ce1b45a3be6fe1ebfc7dbcb71658b46
julianaoddeus <julianaacademi@gmail.com>
2025-06-15 19:11:14 -0300

    feat: implement date availability check in appointment form to enhance scheduling accuracy

--- appointment/src/app/(protected)/appointments/_components/add-appointment-form.tsx
+++ appointment/src/app/(protected)/appointments/_components/add-appointment-form.tsx
@@ -122,6 +122,23 @@ export default function AddAppointmentForm({
       }),
   });
 
+  const isDateAvailable = (date: Date): boolean => {
+    if (!selectedDoctorId) return false;
+
+    const selectedDoctor = doctors.find(
+      (doctor) => doctor.id === selectedDoctorId,
+    );
+
+    if (!selectedDoctor) return false;
+
+    const dayOfWeek = date.getDay();
+
+    return (
+      dayOfWeek >= selectedDoctor?.availableFromWeekDays &&
+      dayOfWeek <= selectedDoctor?.availableToWeekDays
+    );
+  };
+
   return (
     <DialogContent>
       <DialogHeader>
@@ -249,9 +266,10 @@ export default function AddAppointmentForm({
                       selected={field.value}
                       onSelect={field.onChange}
                       disabled={(date) =>
-                        date < new Date() || date < new Date("1900-01-01")
+                        date < new Date() || !isDateAvailable(date)
                       }
                       initialFocus
+                      locale={ptBR}
                     />
                   </PopoverContent>
                 </Popover>
