b915e166338816334253664fa2b5579031cf17cc
julianaoddeus <julianaacademi@gmail.com>
2025-06-23 20:19:53 -0300

    feat: add appointment statistics and revalidation; remove unused SignOutButton component

--- appointment/src/actions/add-appointment/index.ts
+++ appointment/src/actions/add-appointment/index.ts
@@ -78,4 +78,5 @@ export const addAppointment = actionClient
       });
 
     revalidatePath("/appointments");
+    revalidatePath("/dashboard");
   });
--- appointment/src/app/(protected)/dashboard/page.tsx
+++ appointment/src/app/(protected)/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+import { and, count, eq, gte, lte, sum } from "drizzle-orm";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
@@ -10,12 +12,21 @@ import {
   PageHeaderDescription,
   PageHeaderTitle,
 } from "@/components/ui/page-container";
+import { db } from "@/db";
+import { appointmentsTable, doctorsTable, patientsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import { DatePicker } from "./_components/date-picker";
+import StatsCards from "./_components/stats-cards";
 
-const DashboardPage = async () => {
-  //--------* acesso a seção de usuário *----------
+interface DashboardPageProps {
+  searchParams: Promise<{
+    from: string;
+    to: string;
+  }>;
+}
+
+const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
@@ -27,21 +38,77 @@ const DashboardPage = async () => {
   if (!session.user.clinic) {
     redirect("/clinic");
   }
-  //--------* fim do acesso a seção de usuário *----------
+
+  const { from, to } = await searchParams;
+  if (!from || !to) {
+    redirect(
+      `/dashboard?from=${dayjs().format("YYYY-MM-DD")}&to=${dayjs().add(1, "month").format("YYYY-MM-DD")}`,
+    );
+  }
+
+  const [totalRevenues, totalAppointments, totalPatients, totalDoctors] =
+    await Promise.all([
+      db
+        .select({
+          total: sum(appointmentsTable.appointmentPriceInCents),
+        })
+        .from(appointmentsTable)
+        .where(
+          and(
+            eq(appointmentsTable.clinicId, session.user.clinic.id),
+            gte(appointmentsTable.date, new Date(from)),
+            lte(appointmentsTable.date, new Date(to)),
+          ),
+        ),
+      db
+        .select({
+          total: count(),
+        })
+        .from(appointmentsTable)
+        .where(
+          and(
+            eq(appointmentsTable.clinicId, session.user.clinic.id),
+            gte(appointmentsTable.date, new Date(from)),
+            lte(appointmentsTable.date, new Date(to)),
+          ),
+        ),
+      db
+        .select({
+          total: count(),
+        })
+        .from(patientsTable)
+        .where(eq(patientsTable.clinicId, session.user.clinic.id)),
+      db
+        .select({
+          total: count(),
+        })
+        .from(doctorsTable)
+        .where(eq(doctorsTable.clinicId, session.user.clinic.id)),
+    ]);
+
+  const totalRevenuesValue = Number(totalRevenues[0]?.total ?? 0);
+
   return (
     <PageContainer>
       <PageHeader>
         <PageHeaderContent>
           <PageHeaderTitle>Dashboard</PageHeaderTitle>
           <PageHeaderDescription>
-            Acesse as informações do seu consultório, pacientes e muito mais.
+            Tenha informações do seu consultório, pacientes e muito mais.
           </PageHeaderDescription>
         </PageHeaderContent>
         <PageActions>
           <DatePicker />
         </PageActions>
       </PageHeader>
-      <PageContent>testo</PageContent>
+      <PageContent>
+        <StatsCards
+          totalRevenues={totalRevenuesValue}
+          totalAppointments={totalAppointments[0]?.total ?? 0}
+          totalPatients={totalPatients[0]?.total ?? 0}
+          totalDoctors={totalDoctors[0]?.total ?? 0}
+        />
+      </PageContent>
     </PageContainer>
   );
 };
